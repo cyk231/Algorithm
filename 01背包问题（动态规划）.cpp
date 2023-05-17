e718944fd1ed8e88ff1ba2b54ac40d55b978129c
cyk231 <118497901+cyk231@users.noreply.github.com>
2023-05-17 13:27:59 +0800

    Update 01背包问题（动态规划）.cpp

--- 01背包问题（动态规划）.cpp
+++ 01背包问题（动态规划）.cpp
@@ -1,3 +1,5 @@
+// 二维dp
+
 #include<bits/stdc++.h>
 using namespace std; 
 
@@ -21,3 +23,31 @@ int main()
 	} 
 	cout<<dp[m][t];
 }
+
+
+
+
+// 一维dp
+
+// #include<bits/stdc++.h>
+// using namespace std; 
+
+// int t;// 总时间 
+// int m;// 物品数 
+// int w[105];// 每个物品的时间 
+// int v[105];// 每个物品的价值 
+// int dp[1005];
+
+// int main()
+// {
+// 	cin>>t>>m;
+// 	for(int i=1;i<=m;i++) cin>>w[i]>>v[i];
+// 	for(int i=1;i<=m;i++)
+// 	{
+// 		for(int j=t;j>=0;j--)
+// 		{
+// 			if(j>=w[i]) dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
+// 		}
+// 	} 
+// 	cout<<dp[t];
+// }
